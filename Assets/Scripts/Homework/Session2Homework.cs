4d2a1f536f633b53951bc7e1359c587944c1cbaf
Hauu98 <Hauu98@users.noreply.github.com>
2020-11-05 13:56:27 +0800

    201105

--- Assets/Scripts/Homework/Session2Homework.cs
+++ Assets/Scripts/Homework/Session2Homework.cs
@@ -1,25 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DogClass;
 
 public class Session2Homework : MonoBehaviour
 {
+    //Variables
+    Session2Homework_DogClass dog1, dog2, dog3;
+
+
     // Start is called before the first frame update
     void Start()
     {
         //1. Write a function that uses a condition
+        int test = AddTwoNum(10, 20);
+        Debug.Log(test);
 
+        int min = 2;
+        int hour = 1;
+        int test2 = GetTotalSecond(min, hour);
+        Debug.Log(hour + "hour " + "minutes = " + test2 +  "seconds");
 
         //2. Write a for loop
-
         for (int i = 0; i<5; i++ )
         {
             Debug.Log("冲！");
         }
+
         //3. Write a dog class and initialize an instance of the class
-
-
+        dog1 = new Session2Homework_DogClass("小白", 3);
+        dog2 = new Session2Homework_DogClass("小黑", 5);
+        dog3 = new Session2Homework_DogClass("小花", 1);
+    }
+    void Update()
+    {
+        Debug.Log(dog1.GetName());
+        dog1.SetClothing();
     }
 
+    //Function
+    /// <summary>
+    /// Add two int numbers
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    private static int AddTwoNum(int a, int b)
+    {
+        return a + b;
+    }
 
+    /// <summary>
+    /// Minutes transfer to seconds
+    /// </summary>
+    /// <param name="m"></param>
+    /// <returns></returns>
+    private static int GetTotalSecond(int minute)
+    {
+        return minute * 60;
+    }
+    private static int GetTotalSecond(int minute, int hour)
+    {
+        return GetTotalSecond(minute + hour * 60);
+    }
 }
+
---
+++ Assets/Scripts/Homework/Session2Homework_DogClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DogClass
+{
+
+
+//3. Write a dog class and initialize an instance of the class
+public class Session2Homework_DogClass
+{
+	//Variables
+	string name;
+	string color;
+	int age;
+	float weight;
+
+	//Constructor
+	public Session2Homework_DogClass(string _name, int _age)
+	{
+		name = _name;
+		age = _age;
+		SetClothing();
+	}
+
+	//Behaviors (Functions)
+	public string GetName()
+    {
+		return name;
+    }
+
+	public float GetWeight (float _weight)
+    {
+		return weight = _weight;
+    }
+
+	private void Sleep()
+    {
+		//Action
+		//Action
+		//Action
+    }
+
+	public void SetClothing()
+    {
+		//Set some random clothing for dog
+    }
+
+}
+}
