5b0b3322f44c43491232723ec3d13e3ab2847fd8
Hauu98 <Hauu98@users.noreply.github.com>
2021-01-05 21:24:46 +0800

    Homework_HC

---
+++ Assets/Scripts/Homework/Session 4/Session4RayCasting.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session4RayCasting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Press WASD for RayCast, Create Tower to rebuild the spring wall");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Move the cube up
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up*0.01f);
+        }
+
+        // Move the cube down
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * 0.01f);
+        }
+        // Move the cube left
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.left * 0.01f);
+        }
+        // Move the cube right
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * 0.01f);
+        }
+        // First let's get the position from where we are casting the ray
+        Vector3 rayCastPosition = transform.position;
+
+        // Then create a ray in the direction we want to cast
+        Vector3 rayCastDirection = transform.TransformDirection(Vector3.back);
+
+        // Create a variable that stores the information about what we are hitting
+        RaycastHit objectSeenByRay;
+
+        // Cast the ray
+        if (Physics.Raycast(rayCastPosition, rayCastDirection, out objectSeenByRay))
+        {
+            Debug.Log("Object seen: "+ objectSeenByRay.transform.name);
+            Debug.Log("Object position: " + objectSeenByRay.transform.ToString());
+            Debug.Log("Point where ray is hitting the object:  " + objectSeenByRay.point);
+            Destroy(objectSeenByRay.transform.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Homework/Session 4/Session4_HC.cs
+++ Assets/Scripts/Homework/Session 4/Session4_HC.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Session4_2020 : MonoBehaviour
+public class Session4_HC : MonoBehaviour
 {
     //Variables
     public GameObject columnPrefab;
+    bool IsLoading;
     IEnumerator createPrefabsCoroutine;
 
     // Start is called before the first frame update
     void Start()
     {
         createPrefabsCoroutine = DropPrefabs();
+        IsLoading = true;
         // Calling (using) a corutine
         StartCoroutine(createPrefabsCoroutine);
     }
@@ -19,14 +21,20 @@ public class Session4_2020 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        Debug.Log(Time.time);
-        if (Time.time > 3)
+        //1. Time to stop
+        //Debug.Log(Time.time);
+        //if (Time.time > 3)
+        //{
+        //    // Stop a specific coroutine
+        //    StopCoroutine(createPrefabsCoroutine);
+        //    // Stop all coroutines
+        //    StopAllCoroutines();
+        //}
+
+        // 2. Press A to stop
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            // Stop a specific coroutine
-            StopCoroutine(createPrefabsCoroutine);
-            // Stop all coroutines
-            StopAllCoroutines();
+            IsLoading = false;
         }
 
     }
@@ -34,14 +42,16 @@ public class Session4_2020 : MonoBehaviour
     // Coroutines
     IEnumerator DropPrefabs()
     {
-        while (true) 
+        while (IsLoading == true) 
         {
             // Action the corotine (create and drop column)
             Vector3 dropPosition = new Vector3(Random.Range(-5.0f, 12.0f), Random.Range(3.0f, 14.0f), Random.Range(-8.0f, 6.0f));
             Quaternion dropRotation = new Quaternion(Random.Range(0, 90), Random.Range(0, 90), Random.Range(0, 90), 1);
             Instantiate(columnPrefab, dropPosition, dropRotation);
+            Debug.Log("Loading!");
             yield return new WaitForSeconds(0.1f);
-
+           
         }
+        Debug.Log("Finish!");
     }
 }
