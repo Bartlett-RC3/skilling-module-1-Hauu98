89e70236a5b60ee67ccec36897ac23f3f3f2f814
Hauu98 <Hauu98@users.noreply.github.com>
2021-03-12 02:21:32 +0800

    RC9_Unity01HW_HaoyaChen

---
+++ Assets/Scripts/Homework/SolarRotation_Haoya.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class
+public class SolarRotation_Haoya : MonoBehaviour
+{
+    // Variables
+    public float rotationSpeedSelf = 20.0f;
+    public float rotationSpeedSun = 50.0f;
+    public GameObject sun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotation around the planet axis
+        this.gameObject.transform.RotateAround(this.gameObject.transform.position, Vector3.up, rotationSpeedSelf * Time.deltaTime);
+
+        // Rotation around the sun
+        this.gameObject.transform.RotateAround(sun.transform.position, Vector3.up, rotationSpeedSun * Time.deltaTime);
+    }
+}
